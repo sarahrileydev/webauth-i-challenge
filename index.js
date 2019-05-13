b91523f6457e25ae5ac9241fa7c1a92ac0d350c3
sriley <sarahriley.dev@gmail.com>
2019-05-13 15:36:32 -0400

    added dependencies

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const bcrypt = require("bcryptjs");
+
+
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
